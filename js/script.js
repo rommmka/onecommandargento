1a193a15d7ec0f38217d0fa39126d7b44462a63a
Roman Chupryna <rommmmka@gmail.com>
2019-11-19 18:10:18 +0200

    Added && to commands

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ function phpSetting(phpPath) {
 
 function enabledAmp(phpPath, storeId) {
     if (document.getElementById('amp').checked){
-        return phpPath + ' bin/magento swissup:module:install --store=' + storeIdSetting(storeId) + ' Swissup_Amp;';
+        return phpPath + ' bin/magento swissup:module:install --store=' + storeIdSetting(storeId) + ' Swissup_Amp && ';
     }
 }
 
@@ -82,29 +82,29 @@ function generateCode(domainName = 'localhost', licenseKey = 'LICENSE') {
 
     var themeName = document.getElementById("theme-name").value;
 
-    var code = phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' config repositories.swissuplabs composer https://ci.swissuplabs.com/api/packages.json;'+
-        phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' config -a -g http-basic.ci.swissuplabs.com "' + domainName + '" "' + licenseKey + '";' +
-        phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' require swissup/argento-m2;' +
-        phpSetting(phpPath) + ' bin/magento module:enable Swissup_Core Swissup_Ajaxpro Swissup_Ajaxsearch Swissup_Amp Swissup_Askit Swissup_Attributepages Swissup_Compare Swissup_EasySlide Swissup_Easybanner Swissup_Easycatalogimg Swissup_Easytabs Swissup_Fblike Swissup_FeaturedAttributes Swissup_FontAwesome Swissup_Gdpr Swissup_GdprAskit Swissup_GdprTestimonials Swissup_Highlight Swissup_HoverGallery Swissup_Hreflang Swissup_Lightboxpro Swissup_Navigationpro Swissup_ProLabels Swissup_Reviewreminder Swissup_RichSnippets Swissup_Rtl Swissup_SeoCanonical Swissup_SeoCore Swissup_SeoCrossLinks Swissup_SeoHtmlSitemap Swissup_SeoImages Swissup_QuantitySwitcher Swissup_SeoPager Swissup_SeoUrls Swissup_SeoTemplates Swissup_SeoXmlSitemap Swissup_SlickCarousel Swissup_SoldTogether Swissup_Stickyfill Swissup_Suggestpage Swissup_Testimonials Swissup_ThemeEditor Swissup_ThemeEditorArgentoEssence Swissup_ThemeEditorArgentoFlat Swissup_ThemeEditorArgentoForce Swissup_ThemeEditorArgentoLuxury Swissup_ThemeEditorArgentoPure2 Swissup_ThemeEditorArgentoMall Swissup_ThemeEditorArgentoStripes;' +
-        phpSetting(phpPath) + ' bin/magento setup:upgrade;' +
-        'rm -rf pub/static/_requirejs var/view_preprocessed pub/static/frontend/ pub/static/adminhtml/ generated/code/;' +
-        phpSetting(phpPath) + ' bin/magento setup:upgrade;' +
-        phpSetting(phpPath) + ' bin/magento setup:static-content:deploy en_US ' + localeSetting(additionalLocale) + ' -f ;' +
+    var code = phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' config repositories.swissuplabs composer https://ci.swissuplabs.com/api/packages.json && '+
+        phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' config -a -g http-basic.ci.swissuplabs.com "' + domainName + '" "' + licenseKey + '" && ' +
+        phpSetting(phpPath) + ' ' + composerSetting(composerPath) + ' require swissup/argento-m2 && ' +
+        phpSetting(phpPath) + ' bin/magento module:enable Swissup_Core Swissup_Ajaxpro Swissup_Ajaxsearch Swissup_Amp Swissup_Askit Swissup_Attributepages Swissup_Compare Swissup_EasySlide Swissup_Easybanner Swissup_Easycatalogimg Swissup_Easytabs Swissup_Fblike Swissup_FeaturedAttributes Swissup_FontAwesome Swissup_Gdpr Swissup_GdprAskit Swissup_GdprTestimonials Swissup_Highlight Swissup_HoverGallery Swissup_Hreflang Swissup_Lightboxpro Swissup_Navigationpro Swissup_ProLabels Swissup_Reviewreminder Swissup_RichSnippets Swissup_Rtl Swissup_SeoCanonical Swissup_SeoCore Swissup_SeoCrossLinks Swissup_SeoHtmlSitemap Swissup_SeoImages Swissup_QuantitySwitcher Swissup_SeoPager Swissup_SeoUrls Swissup_SeoTemplates Swissup_SeoXmlSitemap Swissup_SlickCarousel Swissup_SoldTogether Swissup_Stickyfill Swissup_Suggestpage Swissup_Testimonials Swissup_ThemeEditor Swissup_ThemeEditorArgentoEssence Swissup_ThemeEditorArgentoFlat Swissup_ThemeEditorArgentoForce Swissup_ThemeEditorArgentoLuxury Swissup_ThemeEditorArgentoPure2 Swissup_ThemeEditorArgentoMall Swissup_ThemeEditorArgentoStripes && ' +
+        phpSetting(phpPath) + ' bin/magento setup:upgrade && ' +
+        'rm -rf pub/static/_requirejs var/view_preprocessed pub/static/frontend/ pub/static/adminhtml/ generated/code/ && ' +
+        phpSetting(phpPath) + ' bin/magento setup:upgrade && ' +
+        phpSetting(phpPath) + ' bin/magento setup:static-content:deploy en_US ' + localeSetting(additionalLocale) + ' -f  && ' +
         phpSetting(phpPath) + ' bin/magento swissup:module:install --store=' + storeIdSetting(storeId) + ' ' +
-        themeName + ' ' + licenseKey + ';' +
+        themeName + ' ' + licenseKey + ' && ' +
         enabledAmp(phpSetting(phpPath), storeId) +
-        'cp -R vendor/swissup/theme-frontend-argento-essence/resources/media/*;' +
-        'cp -R vendor/swissup/theme-frontend-argento-force/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/theme-frontend-argento-flat/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/theme-frontend-argento-pure2/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/theme-frontend-argento-mall/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/theme-frontend-argento-luxury/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/theme-frontend-argento-stripes/resources/media/* pub/media/;'+
-        'cp -R vendor/swissup/module-amp/resources/media/* pub/media/;'+
-        'chmod -R 775 pub/media/easybanner pub/media/easyslide pub/media/highlight pub/media/wysiwyg;';
+        'cp -R vendor/swissup/theme-frontend-argento-essence/resources/media/* && ' +
+        'cp -R vendor/swissup/theme-frontend-argento-force/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/theme-frontend-argento-flat/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/theme-frontend-argento-pure2/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/theme-frontend-argento-mall/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/theme-frontend-argento-luxury/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/theme-frontend-argento-stripes/resources/media/* pub/media/ && '+
+        'cp -R vendor/swissup/module-amp/resources/media/* pub/media/ && '+
+        'chmod -R 775 pub/media/easybanner pub/media/easyslide pub/media/highlight pub/media/wysiwyg && ';
 
         if (document.getElementById('switch-input').checked){
-            code = splitString(code, ';');
+            code = splitString(code, ' && ');
         }
 
         document.getElementById("generated-content").innerHTML = code;
