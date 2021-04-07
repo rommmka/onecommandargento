a85458be41f70b7827d770af490a1895a6c466cb
rommmka <rommmmka@gmail.com>
2021-04-07 17:56:20 +0300

    Added 'client-generator' page to make Best client list items

---
+++ js/generator.js
@@ -0,0 +1,48 @@
+
+// Variables
+/*************************/
+
+var siteUrl = "http://undefined.com";
+var generated = "";
+
+/*************************/
+
+
+function generateCode(){
+	siteUrl = document.querySelector('.site-url').value;
+
+	var url = new URL(siteUrl);
+	var siteName = url.hostname;
+	var imageName = siteName.split(".")[0];
+
+
+	if (siteUrl == 'http://undefined.com') {
+		document.querySelector('#output-field').value = "Enter a Valid URL please";
+		return;
+	}
+
+	document.querySelector('#output-field').value = `
+	<div class="item">
+		<a class="image" rel="nofollow" target="_blank" href="${siteUrl}">
+			<img alt="${siteName}" srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
+			https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/500/${imageName}.png 500w" sizes="(min-width: 1300px) 250px, (min-width: 1025px) 25vw, (min-width: 640px) 33vw, (min-width: 375px) 50vw, 100vw" src="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png" data-src="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png" data-srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
+		https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/500/${imageName}.png 500w" class=" lazyloaded"></a>
+		<div class="info">
+			<a class="title" rel="nofollow" target="_blank" href="${siteUrl}">${siteName}</a>
+			<p class="description">
+
+            	<!-- Description -->
+
+			</p>
+			<div class="tags">
+				<span></span>
+				<span></span>
+
+				<!-- More tags -->
+
+			</div>
+		</div>
+	</div>
+	`;
+	return;
+}
