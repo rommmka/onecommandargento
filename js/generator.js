115ddec89e022eba01bb5bc3664306a1e2330b50
rommmka <rommmmka@gmail.com>
2021-04-08 17:36:10 +0300

    Generator: added some valifdation logics

--- js/generator.js
+++ js/generator.js
@@ -2,47 +2,82 @@
 // Variables
 /*************************/
 
-var siteUrl = "http://undefined.com";
 var generated = "";
 
 /*************************/
 
+function validateUrl (siteUrl) {
+
+}
 
 function generateCode(){
 	siteUrl = document.querySelector('.site-url').value;
+	var buttonRun = document.querySelector('.button-run');
+
+	if (!siteUrl.includes('http')) { siteUrl = "http://" + siteUrl;	} 
+	
+	if (siteUrl == 'http://') {
+		document.querySelector('#output-field').value = "Enter an URL, please";
+		return;
+	}
+
+	if (!siteUrl.includes('.')) { 
+		document.querySelector('#output-field').value = "Enter a valid URL, please";
+		return;
+	}
 
 	var url = new URL(siteUrl);
 	var siteName = url.hostname.replace('www.','');
 	var imageName = siteName.split(".")[0];
 
 
-	if (siteUrl == 'http://undefined.com') {
-		document.querySelector('#output-field').value = "Enter a Valid URL please";
-		return;
-	}
+
+	buttonRun.classList.remove('hover:bg-red-500');
+	buttonRun.classList.add('copied', 'bg-green-400');
+	buttonRun.innerText = "Copied";
 
 	document.querySelector('#output-field').value = `
-	<div class="item">
-		<a class="image" rel="nofollow" target="_blank" href="${siteUrl}">
-			<img alt="${siteName}" srcset="images/clients/250/${imageName}.png 250w,
-			images/clients/500/${imageName}.png 500w" sizes="(min-width: 1300px) 250px, (min-width: 1025px) 25vw, (min-width: 640px) 33vw, (min-width: 375px) 50vw, 100vw" src="images/clients/250/${imageName}.png" data-src="images/clients/250/${imageName}.png" data-srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
-		images/clients/500/${imageName}.png 500w" class=" lazyloaded"></a>
-		<div class="info">
-			<a class="title" rel="nofollow" target="_blank" href="${siteUrl}">${siteName}</a>
-			<p class="description">
+<div class="item">
+	<a class="image" rel="nofollow" target="_blank" href="${siteUrl}">
+		<img alt="${siteName}" srcset="images/clients/250/${imageName}.png 250w,
+		images/clients/500/${imageName}.png 500w" sizes="(min-width: 1300px) 250px, (min-width: 1025px) 25vw, (min-width: 640px) 33vw, (min-width: 375px) 50vw, 100vw" src="images/clients/250/${imageName}.png" data-src="images/clients/250/${imageName}.png" data-srcset="images/clients/250/${imageName}.png 250w,
+	images/clients/500/${imageName}.png 500w" class=" lazyloaded"></a>
+	<div class="info">
+		<a class="title" rel="nofollow" target="_blank" href="${siteUrl}">${siteName}</a>
+		<p class="description">
 
-            	<!-- Description -->
+        	<!-- Description -->
 
-			</p>
-			<div class="tags">
-				<span></span>
-				<span></span>
+		</p>
+		<div class="tags">
+			<span></span>
+			<span></span>
 
-				<!-- More tags -->
+			<!-- More tags -->
 
-			</div>
 		</div>
 	</div>
+</div>
 	`;
+
+	// document.querySelector('#output-field').select();
+
+	if (navigator.permissions) {
+        navigator.permissions.query({name: 'clipboard-write'}).then(function (result) {
+            if (result.state === 'granted' || result.state === 'prompt') {
+                navigator.clipboard.writeText(document.querySelector('#output-field').value);
+            }
+        });
+    }
 	return;
+}
+
+function clearField(){
+	document.querySelector('.site-url').value = "";
+	var buttonRun = document.querySelector('.button-run');
+
+	buttonRun.classList.add('hover:bg-red-500');
+	buttonRun.classList.remove('copied', 'bg-green-400');
+	buttonRun.innerText = "Get code";
+
 }
