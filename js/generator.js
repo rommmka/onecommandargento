4718be0d9157d53956ecae82e9e8ff3011f192c1
rommmka <rommmmka@gmail.com>
2021-04-08 14:20:21 +0300

    Client Generator: Removed firecheckout url relations

--- js/generator.js
+++ js/generator.js
@@ -24,9 +24,9 @@ function generateCode(){
 	document.querySelector('#output-field').value = `
 	<div class="item">
 		<a class="image" rel="nofollow" target="_blank" href="${siteUrl}">
-			<img alt="${siteName}" srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
-			https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/500/${imageName}.png 500w" sizes="(min-width: 1300px) 250px, (min-width: 1025px) 25vw, (min-width: 640px) 33vw, (min-width: 375px) 50vw, 100vw" src="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png" data-src="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png" data-srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
-		https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/500/${imageName}.png 500w" class=" lazyloaded"></a>
+			<img alt="${siteName}" srcset="images/clients/250/${imageName}.png 250w,
+			images/clients/500/${imageName}.png 500w" sizes="(min-width: 1300px) 250px, (min-width: 1025px) 25vw, (min-width: 640px) 33vw, (min-width: 375px) 50vw, 100vw" src="images/clients/250/${imageName}.png" data-src="images/clients/250/${imageName}.png" data-srcset="https://www.firecheckout.net/skin/frontend/argento/firecheckout/images/clients/250/${imageName}.png 250w,
+		images/clients/500/${imageName}.png 500w" class=" lazyloaded"></a>
 		<div class="info">
 			<a class="title" rel="nofollow" target="_blank" href="${siteUrl}">${siteName}</a>
 			<p class="description">
